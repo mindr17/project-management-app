73fde63dab33025b69acb7de22778c59c29c5745
Andreyjkov <Andreyjskov@gmail.com>
2022-11-16 03:39:18 +0300

    feat: add preloader

---
+++ src/components/Preloader/Preloader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Preloader = () => {
+  return (
+    <div className="load" data-testid={'preloaderTest'}>
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+    </div>
+  );
+};
+
+export default Preloader;

025b775cd2eb29f1d1ab0e7ca4b99eadd9b10f3a
Andreyjkov <Andreyjskov@gmail.com>
2022-11-19 06:57:16 +0300

    feat: add stor and slice for profile

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sliceAuth from './sliceAuth';
+import profileSlice from './profile/profileSlice';
 
 const store = configureStore({
   reducer: {
     auth: sliceAuth,
+    profile: profileSlice
 
   },
 });
---
+++ src/store/profile/profileSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ResponsesAuth } from '../authSlice';
+import { deleteUser, updateUser } from './profileThunk';
+
+interface IinitialState {
+  user: ResponsesAuth,
+  isDelete: boolean;
+  isLoading: boolean;
+}
+
+const initialState = {
+  user: {},
+  isDelete: false,
+  isLoading: false,
+} as IinitialState
+
+const profileSlice = createSlice({
+  name: 'profile',
+  initialState,
+
+  reducers: {
+    setIsDelete(state) {
+      state.isDelete = false
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(deleteUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteUser.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isDelete = true
+        console.log('User Delete');
+        state = initialState
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.user = action.payload
+        localStorage.setItem("user", JSON.stringify(action.payload));
+      })
+  },
+});
+
+export const { setIsDelete } = profileSlice.actions;
+
+export default profileSlice.reducer;
+

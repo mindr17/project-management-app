44d1adc13c82268edc255835d3728f719420d0bb
Sergey.Mikhalkin <sergey.mikhalkin.work@gmail.com>
2022-11-29 23:39:51 +0300

    feat: add description to blocks on the welcome page

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -18,12 +18,49 @@ export default function Welcome() {
         <div className={s.wrapper}>
           <section className={s.aboutBlock}>
             <h1 className={s.blockHeader}>Who we are?</h1>
+            <div className={s.teamWrapper}>
+              <div className={s.teammate}>
+                <img src='/avatar.png'></img>
+                <div className={s.teammateInfo}>
+                  <p className={s.name}>Andrey</p>
+                  <p className={s.position}>Developer, Teamlead</p>
+                </div>
+              </div>
+              <div className={s.teammate}>
+                <img src='/avatar.png'></img>
+                <div className={s.teammateInfo}>
+                  <p className={s.name}>Andrey</p>
+                  <p className={s.position}>Developer</p>
+                </div>
+              </div>
+              <div className={s.teammate}>
+                <img src='/avatar.png'></img>
+                <div className={s.teammateInfo}>
+                  <p className={s.name}>Sergey</p>
+                  <p className={s.position}>Developer</p>
+                </div>
+              </div>
+            </div>
           </section>
           <section className={s.descBlock}>
             <h1 className={s.blockHeader}>What our app can?</h1>
+            <div className={s.descriptionWrapper}>
+              <p className={s.description}>This app allow you to create projects and divide them by pices(tasks). 
+                You can create as many project as you want. Moreover, set up your task 
+                lifetime. Our team wish it help you to manage your projects and achieve
+                your goals.</p>
+            </div>
           </section>
           <section className={s.techBlock}>
             <h1 className={s.blockHeader}>What we use?</h1>
+            <div className={s.technologiesWrapper}>
+              <div className={s.technologies}>
+                <img src='/typescript.png' alt='typescript'></img>
+                <img src='/redux.png' alt='redux'></img>
+                <img src='/react.png' alt='react'></img>
+                <img src='/next-js.png' alt='nextjs'></img>
+              </div>
+            </div>
           </section>  
         </div>
       </main>
